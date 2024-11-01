4e1049ace68501e05bfb8cc0bb59d358abaaa96b
unknown <heric.moura@unifesp.br>
2024-11-01 11:03:40 -0300

    estiloalterado

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => (
   <header className="text-center py-6 bg-gradient-to-r from-blue-80 to-white shadow-md">
     <p className="text-gray-800 text-base md:max-w-xl mx-auto">
-      Plataforma gratuita para auxiliar no estudo para concursos em São Paulo. Acesse uma vasta coleção de questões de provas realizadas pela banca Avança SP, totalmente grátis.
+      Plataforma gratuita para te ajudar no estudo para concursos em São Paulo. Acesse aqui uma vasta coleção de questões de provas realizadas pela banca Avança SP, totalmente grátis.
     </p>
   </header>
 );
