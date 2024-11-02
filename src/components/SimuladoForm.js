548c10f86ece87335f87cb57e915e07a2daa4ee4
unknown <heric.moura@unifesp.br>
2024-11-02 16:32:21 -0300

    seila

--- src/components/SimuladoForm.js
+++ src/components/SimuladoForm.js
@@ -1,26 +1,17 @@
-    // SimuladoForm.js
-    import React, { useState, useEffect } from "react";
-    import QuestionCard from "./QuestionCard";
+// SimuladoForm.js
+import React, { useState, useEffect } from "react";
+import QuestionCard from "./QuestionCard";
 
-    const SimuladoForm = () => {
-    const [data, setData] = useState({});
-    const [cargoOptions, setCargoOptions] = useState([]);
-    const [selectedCargo, setSelectedCargo] = useState("");
-    const [numQuestions, setNumQuestions] = useState(1);
-    const [questions, setQuestions] = useState([]);
-    const [totalQuestions, setTotalQuestions] = useState(0);
-    
-    useEffect(() => {
-      fetch(`https://raw.githubusercontent.com/hericmr/ConcurseiraPobre/master/public/provas_com_respostas.json`)
-        .then(response => response.json())
-        .then(data => {
-          setData(data);
-          populateCargoOptions(data);
-          countTotalQuestions(data);
-        })
-        .catch(error => console.error('Erro:', error));
-    }, []);
-    
+const SimuladoForm = () => {
+  const [data, setData] = useState({});
+  const [cargoOptions, setCargoOptions] = useState([]);
+  const [selectedCargo, setSelectedCargo] = useState("");
+  const [numQuestions, setNumQuestions] = useState(1);
+  const [questions, setQuestions] = useState([]);
+  const [totalQuestions, setTotalQuestions] = useState(0);
+
+  useEffect(() => {
+    fetch(`https://raw.githubusercontent.com/hericmr/ConcurseiraPobre/main/public/provas_com_respostas.json`)
 
         .then(response => response.json())
         .then(data => {
@@ -29,42 +20,54 @@
             countTotalQuestions(data);
         })
         .catch(error => console.error('Erro:', error));
-    }, []);
+}, []);
 
-    const populateCargoOptions = (data) => {
-        const uniqueCargos = new Set();
-        Object.entries(data).forEach(([concursoId, categorias]) => {
-        Object.values(categorias).forEach(categoria => {
-            categoria.questoes.forEach(questao => {
-            uniqueCargos.add(questao.cargo);
-            });
+  const populateCargoOptions = (data) => {
+    const uniqueCargos = new Set();
+    Object.entries(data).forEach(([concursoId, categorias]) => {
+      Object.values(categorias).forEach(categoria => {
+        categoria.questoes.forEach(questao => {
+          uniqueCargos.add(questao.cargo);
         });
-        });
-        setCargoOptions([...uniqueCargos]);
-    };
+      });
+    });
+    setCargoOptions([...uniqueCargos]);
+  };
 
-    const countTotalQuestions = (data) => {
-        let count = 0;
-        Object.entries(data).forEach(([concursoId, categorias]) => {
-        Object.values(categorias).forEach(categoria => {
-            count += categoria.questoes.length;
-        });
-        });
-        setTotalQuestions(count);
-    };
+  const countTotalQuestions = (data) => {
+    let count = 0;
+    Object.entries(data).forEach(([concursoId, categorias]) => {
+      Object.values(categorias).forEach(categoria => {
+        count += categoria.questoes.length;
+      });
+    });
+    setTotalQuestions(count);
+  };
 
-    const handleGenerateQuestions = (e) => {
-        e.preventDefault();
-        const filteredData = [];
-        Object.entries(data).forEach(([concursoId, categorias]) => {
-        Object.values(categorias).forEach(categoria => {
-            categoria.questoes.forEach(questao => {
-            if (questao.cargo === selectedCargo) {
-                filteredData.push(questao);
-            }
-            });
+  const handleGenerateQuestions = (e) => {
+    e.preventDefault();
+    const filteredData = [];
+    Object.entries(data).forEach(([concursoId, categorias]) => {
+      Object.values(categorias).forEach(categoria => {
+        categoria.questoes.forEach(questao => {
+          if (questao.cargo === selectedCargo) {
+            filteredData.push(questao);
+          }
         });
-        });
+      });
+    });
+
+    const selectedQuestions = [];
+    for (let i = 0; i < Math.min(numQuestions, filteredData.length); i++) {
+      const randomIndex = Math.floor(Math.random() * filteredData.length);
+      selectedQuestions.push(filteredData[randomIndex]);
+      filteredData.splice(randomIndex, 1);
+    }
+    setQuestions(selectedQuestions);
+
+    // Atualiza o total de questões após cada busca
+    countTotalQuestions(data);
+  };
 
   return (
     <section className="container mx-auto mt-8">
@@ -112,4 +115,4 @@
   );
 };
 
-    export default SimuladoForm;
+export default SimuladoForm;
