8e93d979b0e939c82e87f03cbc383e471076ad13
unknown <heric.moura@unifesp.br>
2024-11-06 10:03:15 -0300

    alterando estilo

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = () => (
       {/* Texto com destaque */}
       <div className="flex flex-col items-center md:min-h-[150px] lg:flex-1 text-center">
         <div className="max-w-[630px] flex flex-col gap-3 pt-6 md:py-8">
-          <h1 className="text-primary-50 text-3xl font-semibold italic md:text-4xl text-black">
+          <h1 className="text-primary-10 text-3xl font-semibold italic md:text-4xl text-black">
 
-            QUESTÕES AVANÇA SP - ACESSE GRÁTIS!
+            Questões da banca AVANÇA SP - ACESSE GRÁTIS!
           </h1>
           <p className="text-primary-50 text-sm font-normal md:text-lg text-black">
             A melhor maneira de se preparar para concursos é praticando com provas reais da banca! Conheça a abordagem da Avança SP e domine os conteúdos exigidos. Esse site é 100% gratuito e oferece uma vasta coleção de questões para turbinar seus estudos em São Paulo.
